ae54b0dd3ff13a3156ffad8de0cb84a5c648e35f
mateusschemer <mateus.schemer@gmail.com>
2022-05-09 15:52:12 -0300

    injeçaõ de dependências

--- Libpythonpro/spam/main.py
+++ Libpythonpro/spam/main.py
@@ -3,5 +3,13 @@ class EnviadorDeSpam:
         self.sessao = sessao
         self.enviador = enviador
 
-    def enviar_email(self, remetente, assunto, corpo):
-        pass
+
+    def enviar_emails(self, remetente, assunto, corpo):
+        for usuario in self.sessao.listar():
+            self.enviador.enviar(
+                remetente,
+                usuario.email,
+                assunto,
+                corpo
+            )
+
--- Libpythonpro/tests/test_spam/test_usuario.py
+++ Libpythonpro/tests/test_spam/test_usuario.py
@@ -2,14 +2,15 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Mateus')
+    usuario = Usuario(nome='Mateus', email= 'mateus.schemer@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 
 def test_listar_usuarios (sessao):
-    usuarios = [Usuario(nome='Mateus'), Usuario(nome='Karin')]
+    usuarios = [Usuario(nome='Mateus', email='mateus.schemer@gmail.com'),
+                Usuario(nome='Karin', email='karin.luccas.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
